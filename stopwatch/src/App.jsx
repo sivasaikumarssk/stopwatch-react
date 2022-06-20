e0ea7c981ebe8b786947096017d7608f1d305e01
sivasaikumar <sivasaikumar.pinjala@gmail.com>
2022-06-21 00:06:21 +0530

    timer functionality done

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
+  const endTime = new Date("June 22,2022 00:00:00").getTime();
+  const [currentTime,setCurrentTime] = useState(new Date().getTime())
+  const gap = endTime - currentTime;
+
+  const seconds = 1000;
+  const min = seconds* 60;
+  const hours = min* 60;
+  const days = hours * 24;
+
+  const remDays = Math.floor(gap/days);
+  const remHours = Math.floor((gap % days)/hours);
+  const remMin = Math.floor((gap% hours)/ min);
+  const remSec = Math.floor((gap % min)/ seconds);
+
+  useEffect(()=>{
+    setTimeout(()=>setCurrentTime(new Date().getTime()),1000)
+  },[currentTime])
+
+
+  
   const [count, setCount] = useState(0)
 
   const regulatorFunc = (e)=>{
@@ -33,7 +54,11 @@ function App() {
       <br />
       <hr/>
       <h2>Stopwatch - React</h2>
-      
+      <h2>days: {remDays}</h2>
+      <h2>hours : {remHours}</h2>
+      <h2>minutes : {remMin}</h2>
+      <h2>seconds: {remSec}</h2>
+      <button >stop timer</button>
     </div>
   )
 }
